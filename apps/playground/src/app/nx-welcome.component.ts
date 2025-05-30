b13626cae53d66cb76d3547e9cbb48ba5b2337e2
Ricardo Smarzaro <ricardo.smarzaro@aveva.com>
2025-05-30 10:19:04 +0200

    Update tsconfig paths to use new package names and integrate SmzCoreComponent in playground component template.

--- apps/playground/src/app/nx-welcome.component.ts
+++ apps/playground/src/app/nx-welcome.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SmzCoreComponent } from '@smz-ui/core';
 @Component({
   selector: 'app-nx-welcome',
-  imports: [CommonModule ],
+  imports: [CommonModule, SmzCoreComponent],
   template: `
   <div>Playground Works!</div>
+  <lib-smz-core></lib-smz-core>
   `,
   styles: [],
   encapsulation: ViewEncapsulation.None,
