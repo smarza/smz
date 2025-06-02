c99895c17564e83fcf2877a7c24de83ffc6ea419
rsmarzaro <rsmarzaro@tecgraf.puc-rio.br>
2025-06-02 18:16:42 +0200

    feat(posts-crud): enhance posts CRUD demo with post count display and code improvements

--- apps/playground/src/app/layout/static-data.ts
+++ apps/playground/src/app/layout/static-data.ts
@@ -9,6 +9,7 @@ export const INITIAL_SIDEBAR: MenuItem[] = [
             { label: 'Feature Store Demo 1 (Provided in the Route)', icon: 'pi pi-fw pi-star', routerLink: ['/feature-store-1'] },
             { label: 'Feature Store Demo 2 (Provided in the Component)', icon: 'pi pi-fw pi-star', routerLink: ['/feature-store-2'] },
             { label: 'Global Store Demo', icon: 'pi pi-fw pi-database', routerLink: ['/global-store'] },
+            { label: 'Posts CRUD Demo', icon: 'pi pi-fw pi-database', routerLink: ['/posts-crud'] },
         ]
     }
 ]
