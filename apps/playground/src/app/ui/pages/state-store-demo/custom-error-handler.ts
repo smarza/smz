eae9d97a7acde699496eb51c8dc70d5641dc76f2
rsmarzaro <rsmarzaro@tecgraf.puc-rio.br>
2025-06-16 16:13:55 +0200

    refactor(error-handler): remove unused imports to streamline custom error handler

--- apps/playground/src/app/ui/pages/state-store-demo/custom-error-handler.ts
+++ apps/playground/src/app/ui/pages/state-store-demo/custom-error-handler.ts
@@ -1,6 +1,4 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ApiError } from '@smz-ui/core';
 
 @Injectable({
   providedIn: 'root'
